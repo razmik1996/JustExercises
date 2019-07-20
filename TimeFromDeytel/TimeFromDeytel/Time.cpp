226ea4b334bb766a36bdb84caf569b0277ea6cbe
razmik1996 <razfreelancer@mail.ru>
2019-07-20 23:38:18 +0400

    class Time: added realization and test

--- TimeFromDeytel/TimeFromDeytel/Time.cpp
+++ TimeFromDeytel/TimeFromDeytel/Time.cpp
@@ -1,9 +1,29 @@
 #include "Time.h"
-
+#include <iostream>
+#include <iomanip>
 
 
 Time::Time()
 {
+	hour = minute = second = 0;
+}
+
+void Time::setTime(int h, int m, int s)
+{
+	hour = (h >= 0 && h < 24) ? h : 0;
+	minute = (m >= 0 && m < 60) ? m : 0;
+	second = (s >= 0 && s < 60) ? s : 0;
+}
+
+void Time::printUniversal()
+{
+	std::cout << std::setfill('0') << std::setw(2) << hour << ":" << minute << ":" << second;
+}
+
+void Time::printStandart()
+{
+	std::cout << ((hour == 0 || hour == 12) ? 12 : hour % 12) << ":" << std::setfill('0') <<
+		std::setw(2) << minute << ":" << std::setw(2) << second << ((hour < 12) ? " AM" : " PM");
 }
 
 
